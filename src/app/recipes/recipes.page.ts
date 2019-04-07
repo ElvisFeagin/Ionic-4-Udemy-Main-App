6df82b56bcac95e7015042d5355f5f16349918fc
Elvis Feagin <47120736+ElvisFeagin@users.noreply.github.com>
2019-04-07 11:43:50 -0700

    91 - Debugging Android Apps

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -14,6 +14,7 @@ export class RecipesPage implements OnInit {
 
   ngOnInit() {
     this.recipes = this.recipesService.getAllRecipes();
+    console.log('~~~~~~~~ LOADED RECIPES')
     console.log(this.recipes);
   }
 }
