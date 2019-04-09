3694ff188a7367877443854e80deb616f51f4d0e
Elvis Feagin <47120736+ElvisFeagin@users.noreply.github.com>
2019-04-09 11:16:07 -0700

    104 - Outputting 'Places'

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -8,15 +8,15 @@ export class PlacesService {
   private _places: Place[] = [
     new Place(
       'p1',
-      "L'Amour Toujours",
-      'A Romantic place in Paris!',
+      'Manhattan Mansion',
+      'In the heart of New York City.',
       'https://lonelyplanetimages.imgix.net/mastheads/GettyImages-538096543_medium.jpg?sharp=10&vib=20&w=1200',
       149.99
     ),
     new Place(
       'p2',
-      'Manhattan Mansion',
-      'In the heart of New York City.',
+      "L'Amour Toujours",
+      'A romantic place in Paris!',
       'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Paris_Night.jpg/1024px-Paris_Night.jpg',
       189.99
     ),
@@ -24,7 +24,7 @@ export class PlacesService {
       'p3',
       'The Foggy Palace',
       'Not your average city trip!',
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/San_Francisco_with_two_bridges_and_the_fog.jpg/1024px-San_Francisco_with_two_bridges_and_the_fog.jpg',
+      'https://upload.wikimedia.org/wikipedia/commons/0/01/San_Francisco_with_two_bridges_and_the_fog.jpg',
       99.99
     )
   ];
