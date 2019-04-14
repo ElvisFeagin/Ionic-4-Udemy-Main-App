f26af20a6fb778a92dd3ff32271558e4bb208169
Elvis Feagin <47120736+ElvisFeagin@users.noreply.github.com>
2019-04-14 09:15:24 -0700

    135 - Adding a Spinner

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+
 import { AuthService } from './auth.service';
 
 @Component({
@@ -8,12 +9,18 @@ import { AuthService } from './auth.service';
   styleUrls: ['./auth.page.scss']
 })
 export class AuthPage implements OnInit {
+  isLoading = false;
+
   constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit() {}
 
   onLogin() {
     this.authService.login();
-    this.router.navigateByUrl('/places/tabs/discover');
+    this.isLoading = true;
+    setTimeout(() => {
+      this.isLoading = false;
+      this.router.navigateByUrl('/places/tabs/discover');
+    }, 1500);
   }
 }
