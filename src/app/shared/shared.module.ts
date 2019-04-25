581bc111139718e3d8e0f8adee0bbdeab5f7a9b3
Elvis Feagin <47120736+ElvisFeagin@users.noreply.github.com>
2019-04-25 16:31:06 -0700

    207 - Opening the Map Modal

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
 
 import { MapModalComponent } from './map-modal/map-modal.component';
 import { LocationPickerComponent } from './pickers/location-picker/location-picker.component';
 
 @NgModule({
   declarations: [LocationPickerComponent, MapModalComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, IonicModule],
   exports: [LocationPickerComponent, MapModalComponent],
   entryComponents: [MapModalComponent]
 })
